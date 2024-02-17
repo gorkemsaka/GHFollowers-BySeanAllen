67ad3ebf30ddb1ea9dcb34c80ceaf714675bf168
Gorkem Saka <gorkemsakaa@icloud.com>
2024-02-17 17:24:09 +0300

    UserModel changed, userFetch added

--- GHFollowers-BySeanAllen/Managers/NetworkManager.swift
+++ GHFollowers-BySeanAllen/Managers/NetworkManager.swift
@@ -50,4 +50,41 @@ class NetworkManager {
         }
         task.resume()
     }
+    
+    
+    func fetchUser(username: String, completed: @escaping (Result<User, Theme.GFError>) -> Void){
+        let endPoint = baseURL + "\(username)"
+        
+        guard let url = URL(string: endPoint) else {
+            completed(.failure(.urlErrorTitle))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){ data, response, error in
+            if let _ = error {
+                completed(.failure(.requestErrorTitle))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.responseErrorTtitle))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.dataErrorTitle))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completed(.success(user))
+            } catch {
+                completed(.failure(.dataErrorTitle))
+            }
+        }
+        task.resume()
+    }
 }
--- GHFollowers-BySeanAllen/Views/User Info View/UserInfoViewController.swift
+++ GHFollowers-BySeanAllen/Views/User Info View/UserInfoViewController.swift
@@ -25,6 +25,18 @@ class UserInfoViewController: UIViewController {
     private func configure(){
         view.backgroundColor = .systemBackground
         configureBarbuttonItem()
+        NetworkManager.shared.fetchUser(username: username) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let user) :
+                print(user)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", bodyTitle: error.rawValue, buttonTitle: "Ok")
+            }
+            
+        }
     }
 }
 
